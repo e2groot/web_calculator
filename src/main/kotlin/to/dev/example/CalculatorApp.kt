9b4f6c8bb7290c40a9aa6ea26a3c3a1b61c6f2e6
Ev Spin <eespino3@alumni.jh.edu>
2019-01-17 01:08:19 -0500

    updated git ignore

---
+++ src/main/kotlin/to/dev/example/CalculatorApp.kt
@@ -0,0 +1,12 @@
+package to.dev.example
+
+import io.dropwizard.Application
+import io.dropwizard.setup.Environment
+
+class CalculatorApp : Application<CalculatorConfig>() {
+    override fun run(configuration: CalculatorConfig, environment: Environment) {
+        println("Running ${configuration.name}!")
+        val calculatorComponent = CalculatorComponent()
+        environment.jersey().register(calculatorComponent)
+    }
+}
---
+++ src/main/kotlin/to/dev/example/MainApp.kt
@@ -0,0 +1,6 @@
+package to.dev.example
+
+fun main(args: Array<String>) {
+    CalculatorApp().run(*args)
+}
+
